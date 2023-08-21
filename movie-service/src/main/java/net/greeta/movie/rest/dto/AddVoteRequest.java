8c49e565256cc40db4232c72b4ee51594bb598bd
skyglass <skyglass2001@gmail.com>
2023-08-21 18:37:02 +0200

    fixed score field

--- movie-service/src/main/java/net/greeta/movie/rest/dto/AddVoteRequest.java
+++ movie-service/src/main/java/net/greeta/movie/rest/dto/AddVoteRequest.java
@@ -2,12 +2,13 @@ package net.greeta.movie.rest.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class AddVoteRequest {
 
     @Schema(example = "10")
-    @NotBlank
+    @Positive
     private int score;
 }
